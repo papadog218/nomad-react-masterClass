3362133a73c9ad310ef270312121c76e05ca2383
papadog218 <mki218@naver.com>
2022-05-15 16:53:51 +0900

    #2.4 완료

--- react-styled-components/src/App.js
+++ react-styled-components/src/App.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { keyframes } from 'styled-components';
 
 const Father = styled.div`
   display: flex;
@@ -44,35 +45,75 @@ const Father = styled.div`
 //   border-radius: 15px;
 // `;
 // 특정 태그들에 동일한 속성값을 주고 싶을 때
-const Input = styled.input.attrs({required: true, minLength: 10})`
+// const Input = styled.input.attrs({required: true, minLength: 10})`
+//   background-color: tomato;
+// `;
+
+// #2.4
+const Warpper = styled.div`
+  display: flex;
+`;
+const rotateAni = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAni} 1s linear infinite;
+  span {
+   font-size: 50px;
+   &:hover { // &로 자기 자신의 이름을 가져올수있음
+    font-size: 100px;
+   }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      {/* #2.1 */}
-      {/* <BoxOne>
-         <Text>Hello</Text>
-       </BoxOne>
-       <BoxTwo/> */}
+    // <Father>
+    //   {/* #2.1 */}
+    //   {/* <BoxOne>
+    //      <Text>Hello</Text>
+    //    </BoxOne>
+    //    <BoxTwo/> */}
 
-      {/* #2.2 */}
-      {/* <Box bgColor="teal"/> */}
-      {/* <Box bgColor="tomato"/> */}
-      {/* <Circle bgColor="tomato"/> */}
+    //   {/* #2.2 */}
+    //   {/* <Box bgColor="teal"/> */}
+    //   {/* <Box bgColor="tomato"/> */}
+    //   {/* <Circle bgColor="tomato"/> */}
 
-       {/* #2.3 */}
-       {/* as 속성으로 styled의 태그명을 바꿀수있음 */}
-       {/* <Btn>Log in</Btn>
-       <Btn as='a'>Log in</Btn> */}
-       {/* 특정 태그들에 동일한 속성값을 주고 싶을 때 */}
-       <Input/>
-       <Input/>
-       <Input/>
-       <Input/>
-       <Input/>
-    </Father>
+    //    {/* #2.3 */}
+    //    {/* as 속성으로 styled의 태그명을 바꿀수있음 */}
+    //    {/* <Btn>Log in</Btn>
+    //    <Btn as='a'>Log in</Btn> */}
+    //    {/* 특정 태그들에 동일한 속성값을 주고 싶을 때 */}
+    //    <Input/>
+    //    <Input/>
+    //    <Input/>
+    //    <Input/>
+    //    <Input/>
+    // </Father>
+
+    // #2.4
+    <Warpper>
+      <Box>
+        <span>😁</span>
+      </Box>
+    </Warpper>
   );
 }
 
