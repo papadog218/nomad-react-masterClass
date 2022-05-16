b63485f2e5187557d5d0f16371a0e66dd6a768db
papadog218 <mki218@naver.com>
2022-05-16 16:41:48 +0900

    #3.5 완료

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,11 +1,24 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [val, setVal] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value}
+    } = e;
+    setVal(value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`hello`, val);
+  };
   return (
    <div>
-     <Circle yhBDColor='yellow' yhBGColor='teal'/>
-     <Circle text='im here' yhBGColor='tomato'/>
+     <form onSubmit={onSubmit}>
+       <input type="text" placeholder="username" value={val} onChange={onChange} />
+       <button>Login</button>
+     </form>
    </div>
   );
 }
