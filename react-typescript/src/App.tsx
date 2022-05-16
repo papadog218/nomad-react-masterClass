d2254833bb12e66c2bd31240a37f7df07c28aa7a
papadog218 <mki218@naver.com>
2022-05-16 16:04:18 +0900

    #3.3 완료

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -4,8 +4,8 @@ import Circle from "./Circle";
 function App() {
   return (
    <div>
-     <Circle yhBG='teal'/>
-     <Circle yhBG='tomato'/>
+     <Circle yhBDColor='yellow' yhBGColor='teal'/>
+     <Circle text='im here' yhBGColor='tomato'/>
    </div>
   );
 }
--- react-typescript/src/Circle.tsx
+++ react-typescript/src/Circle.tsx
@@ -2,21 +2,27 @@ import styled from "styled-components";
 
 type ContainerProps = {
     bgColor: string;
+    borderColor: string;
 };
 const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 100px;
+    border: 1px solid ${(props) => props.borderColor};
 `;
 
 type CircleProps = {
-    yhBG: string;
+    yhBGColor: string;
+    yhBDColor?: string;
+    text?: string;
 };
 
-function Circle ({yhBG}: CircleProps) {
+function Circle ({yhBGColor, yhBDColor, text = "default text"}: CircleProps) {
     return (
-        <Container bgColor={yhBG}></Container>
+        <Container bgColor={yhBGColor} borderColor={yhBDColor ?? yhBGColor}>
+            {text}
+        </Container>
     );
 }
 
