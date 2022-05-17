214827dc31680247d4bb799a5c20da22adfbf44f
papadog218 <mki218@naver.com>
2022-05-17 17:57:23 +0900

    #4.3 완료

--- react-typescript/src/index.tsx
+++ react-typescript/src/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
--- react-typescript/src/routes/Coins.tsx
+++ react-typescript/src/routes/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 const Header = styled.header`
     height: 10vh;
@@ -33,50 +36,49 @@ const Title = styled.h1`
     color: ${(porps) => porps.theme.accentColor};
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-]
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            const res =  await fetch('https://api.coinpaprika.com/v1/coins');
+            const json = await res.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();
+    }, []);
     return (
         <Container>
             <Header>
                 <Title>Coins</Title>
             </Header>
-            <CoinsList>
-                {coins.map((coin) => (
-                    <Coin key={coin.id}>
-                        {/* a를 쓰면 새로고침이 되니깐 안되게하려고 <Link>를 사용함 */}
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-                    </Coin>
-                ))}
-            </CoinsList>
+            {loading ? <Loader>'Loading...'</Loader>
+            : (
+                <CoinsList>
+                    {coins.map((coin) => (
+                        <Coin key={coin.id}>
+                            {/* a를 쓰면 새로고침이 되니깐 안되게하려고 <Link>를 사용함 */}
+                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                        </Coin>
+                    ))}
+                </CoinsList>
+            )}
         </Container>
     );
 }
