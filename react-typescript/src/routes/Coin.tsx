5a7a034c41d96c7bb7b5adf024267c13c3ea6789
papadog218 <mki218@naver.com>
2022-05-17 16:27:36 +0900

    Revert "Revert "#4.0 완료""
    This reverts commit 5744f6f381519d46f8fd88be4ef1f713ec9cbfdd.

--- react-typescript/src/routes/Coin.tsx
+++ react-typescript/src/routes/Coin.tsx
@@ -1,9 +1,13 @@
 import { useParams } from "react-router-dom";
 
+// interface 를 type 으로 변경하니 잘됨
+type RouteParams = {
+    coinId: string;
+}
+
 function Coin() {
-    const coinId = useParams();
-    console.log(coinId);
-    return (<h1>Coin</h1>);
+    const {coinId} = useParams<RouteParams>();
+    return <h1>Coin: {coinId}</h1>;
 }
 
 export default Coin;
