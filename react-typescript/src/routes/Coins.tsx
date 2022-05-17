de3d9fbaa103f8cc69c294f14ecf8eddde9dbc8c
papadog218 <mki218@naver.com>
2022-05-17 17:30:47 +0900

    #4.2 완료

--- react-typescript/src/routes/Coins.tsx
+++ react-typescript/src/routes/Coins.tsx
@@ -1,12 +1,83 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+    padding: 0 20px;
+`;
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+    background-color: #fff;
+    color: ${(props) => props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    a {
+        transition: color .2s ease-in;
+        display: block;
+        padding: 20px;
+    }
+    &:hover {
+        a {
+            color: ${(props) => props.theme.accentColor};
+        }
+    }
+`;
+
 const Title = styled.h1`
+    font-size: 48px;
     color: ${(porps) => porps.theme.accentColor};
 `;
 
+const coins = [
+    {
+        id: "btc-bitcoin",
+        name: "Bitcoin",
+        symbol: "BTC",
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "eth-ethereum",
+        name: "Ethereum",
+        symbol: "ETH",
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: "coin",
+    },
+    {
+        id: "hex-hex",
+        name: "HEX",
+        symbol: "HEX",
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: "token",
+    },
+]
+
 function Coins() {
     return (
-        <Title>Coins</Title>
+        <Container>
+            <Header>
+                <Title>Coins</Title>
+            </Header>
+            <CoinsList>
+                {coins.map((coin) => (
+                    <Coin key={coin.id}>
+                        {/* a를 쓰면 새로고침이 되니깐 안되게하려고 <Link>를 사용함 */}
+                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                    </Coin>
+                ))}
+            </CoinsList>
+        </Container>
     );
 }
 
