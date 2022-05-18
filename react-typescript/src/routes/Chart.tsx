c8f0078a4a831c81a20ff148e09a469a47ee2bc1
papadog218 <mki218@naver.com>
2022-05-18 16:40:12 +0900

    #4.14 완료(나중에 차트 바꿔보기)

--- react-typescript/src/routes/Chart.tsx
+++ react-typescript/src/routes/Chart.tsx
@@ -62,9 +62,68 @@ function Chart() {
                                 axisBorder: { show: false },
                                 axisTicks: { show: false },
                                 labels: { show: false },
+                                type: 'datetime',
+                                categories: data?.map((price) => price.time_close) as string[],
+                            },
+                            fill: {
+                                type: 'gradient',
+                                gradient: {gradientToColors: ['#0be881'], stops: [0, 100]}
+                            },
+                            colors: ['#0fbcf9'],
+                            tooltip: {
+                                y: {
+                                    formatter: (val) => `$${val.toFixed(2)}`,
+                                }
                             },
                         }}
                     />
+                    // <ReactApexChart
+                    //     type="line"
+                    //     series={[
+                    //         {
+                    //             name: "Price",
+                    //             data: data?.map((price) => price.close) as number[],
+                    //         },
+                    //     ]}
+                    //     options={{
+                    //         theme: {
+                    //             mode: "dark",
+                    //         },
+                    //         chart: {
+                    //             height: 300,
+                    //             width: 500,
+                    //             toolbar: {
+                    //             show: false,
+                    //             },
+                    //             background: "transparent",
+                    //         },
+                    //         grid: { show: false },
+                    //         stroke: {
+                    //             curve: "smooth",
+                    //             width: 4,
+                    //         },
+                    //         yaxis: {
+                    //             show: false,
+                    //         },
+                    //         xaxis: {
+                    //             axisBorder: { show: false },
+                    //             axisTicks: { show: false },
+                    //             labels: { show: false },
+                    //             type: 'datetime',
+                    //             categories: data?.map((price) => price.time_close) as string[],
+                    //         },
+                    //         fill: {
+                    //             type: 'gradient',
+                    //             gradient: {gradientToColors: ['blue'], stops: [0, 100]}
+                    //         },
+                    //         colors: ['red'],
+                    //         tooltip: {
+                    //             y: {
+                    //                 formatter: (val) => `$${val.toFixed(2)}`,
+                    //             }
+                    //         },
+                    //     }}
+                    // />
             }
         </div>
     );
