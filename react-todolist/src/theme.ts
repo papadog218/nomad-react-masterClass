f6c8c23bb3f57ce03b6ebb42d990ec76bfd0da9e
papadog218 <mki218@naver.com>
2022-05-18 23:40:31 +0900

    create todoList & base setting

---
+++ react-todolist/src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme:DefaultTheme = {
+    bgColor: '#2f3640',
+    txtColor: '#fff',
+    accentColor: '#4cd137',
+    cardBgColor: 'transparent'
+}
+export const lightTheme:DefaultTheme = {
+    bgColor: 'whitesmoke',
+    txtColor: '#000',
+    accentColor: '#4cd137',
+    cardBgColor: '#fff'
+}
