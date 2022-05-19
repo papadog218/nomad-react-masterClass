7e3d896976d29972fb8c4c61b96f697b38b2145e
papadog218 <mki218@naver.com>
2022-05-19 19:08:22 +0900

    #5.13 fin

--- react-todolist/src/components/ToDo.tsx
+++ react-todolist/src/components/ToDo.tsx
@@ -1,12 +1,39 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({text}: IToDo) {
+function ToDo({text, category, id}: IToDo) {
+
+    // 하나의 클릭이벤트 함수로 여러 이벤트 처리하기 1. 익명함수의 인자값 사용하기
+    // const onClick = (newCategory: IToDo['category']) => {
+    //     console.log('i wanna to ', newCategory);
+    // };
+    
+    // 하나의 클릭이벤트 함수로 여러 이벤트 처리하기 2. 태그 네임 활용하기
+    const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        // e.currentTarget.name; // 기본
+        const {
+            currentTarget: {name},
+        } = e; // 섹시버전
+        console.log('i wanna to ', name);
+    };
+
+    // 수정기능 추가
+    const setToDos = useSetRecoilState(toDoState);
+
     return (
         <li>
             <span>{text}</span>
-            <button>Doing</button>
-            <button>To Do</button>
-            <button>Done</button>
+            {/* 이벤트에 실행될 함수에 인자값을 받으려면 화살표 함수를 사용해야한다 */}
+            {/* 하나의 클릭이벤트 함수로 여러 이벤트 처리하기 1. 익명함수의 인자값 사용하기 */}
+            {/* {category !== 'DOING' && <button onClick={() => onClick('DOING')}>Doing</button>}
+            {category !== 'TO_DO' && <button onClick={() => onClick('TO_DO')}>To Do</button>}
+            {category !== 'DONE' && <button onClick={() => onClick('DONE')}>Done</button>} */}
+
+            {/* 하나의 클릭이벤트 함수로 여러 이벤트 처리하기 2. 태그 네임 활용하기 */}
+            {category !== 'DOING' && <button name="DOING" onClick={onClick}>Doing</button>}
+            {category !== 'TO_DO' && <button name="TO_DO" onClick={onClick}>To Do</button>}
+            {category !== 'DONE' && <button name="DONE" onClick={onClick}>Done</button>}
         </li>
     );
 }
