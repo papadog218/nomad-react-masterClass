b5a3a1fd56d30a205010cbf7d2c8dc6eef88ad3d
papadog218 <mki218@naver.com>
2022-05-20 20:02:09 +0900

    #6.0 fin

--- react-trelloClone/src/App.tsx
+++ react-trelloClone/src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { minuteState } from "./atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
   const onChangeMinutes = (e: React.FormEvent<HTMLInputElement>) => {
-    setMinutes(e.currentTarget.value);
+    setMinutes(+e.currentTarget.value);
   }
+  const hours = useRecoilValue(hourSelector);
 
   return (
     <div>
       <input value={minutes} onChange={onChangeMinutes} type="number" placeholder="Minutes" />
-      <input type="number" placeholder="Hours" />
+      <input type="number" placeholder="Hours" value={hours} />
     </div>
   );
 }
